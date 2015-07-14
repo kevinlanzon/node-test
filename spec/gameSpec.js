a67d32cb7e28ada29edafa4bb9d668b8d794c6c1
Kevin Lanzon <kevinlanzon@gmail.com>
2015-07-14 15:29:55 +0100

    sets up first failing test

---
+++ spec/gameSpec.js
@@ -0,0 +1,13 @@
+var Game = require('../js/game');
+
+describe('Gutter game', function () {
+
+  var game = new Game();
+
+  it('scores 0 on a gutter game', function () {
+    for(var i = 0; i < 20; i++) {
+      game.roll(0);
+    }
+    expect(game.score).toEqual(0);
+  });
+});
